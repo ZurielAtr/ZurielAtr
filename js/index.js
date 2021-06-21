8d62f4ab47b73d1e2ab2fcb15f1a3be490e6e45d
Zuriel <zurielamayo.gmail.com>
2021-06-20 21:38:57 -0500

    ZurielAtr

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('.img');
+const contenedorimage = document.querySelector('.contenedor-img');
+const imagecontenedor = document.querySelector('.img-show');
+const closeModal = document.querySelector('.bx.bx-x');
+
+images.forEach(image =>{
+    image.addEventListener('click', ()=>{
+        addimage(image.getAttribute('src'));
+    })
+})
+
+const addimage = (src)=> {
+    contenedorimage.classList.toggle('move');
+    imagecontenedor.classList.toggle('show');
+    imagecontenedor.src = src;
+}
+
+closeModal.addEventListener('click', ()=>{
+    contenedorimage.classList.toggle('move');
+    imagecontenedor.classList.toggle('show');
+})
